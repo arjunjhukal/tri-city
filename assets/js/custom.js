7baee37fb502837c84fb7c6573f42741a4425c98
jhukal9@gmail.com <jhukal9@gmail.com>
2024-08-27 20:57:57 +0545

    updated the headline animaton based on content width

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,37 +1,68 @@
 $(function () {
+  $(".banner-scroll-indicator").click(function () {
+    window.scrollTo({
+      top: window.scrollY + 200,
+      behavior: "smooth",
+    });
+  });
+
   $(".timeline-tabs li").each(function (index) {
     $(this).click(function () {
-      $(".timeline-tabs li").each(function () {
-        $(this).removeClass("active");
-      });
-
+      $(".timeline-tabs li").removeClass("active");
       $(this).addClass("active");
 
-      $(".timeline .tab-content-box").each(function () {
-        $(this).removeClass("active");
-      });
-
+      $(".timeline .tab-content-box").removeClass("active");
       $(".timeline .tab-content-box").eq(index).addClass("active");
     });
   });
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Force reflow and calculate the correct width after everything is loaded
+  // $(window).on("load", function () {
+  //   const headlineWidth = $(".headline-content h1").outerWidth();
+  //   console.log("Correct h1 Width:", headlineWidth);
+
+  //   const t1 = gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: ".headline",
+  //       start: "top center",
+  //       end: "+=100%",
+  //       scrub: 1,
+  //       pin: true,
+  //       ease: "ease-in-out",
+  //       markers: {
+  //         startColor: "green",
+  //         endColor: "yellow",
+  //       },
+  //     },
+  //   });
+
+  //   t1.fromTo(".headline-content h1", { x: "50px" }, { x: `-${headlineWidth}px` });
+  // });
 });
 
 $(function () {
-  gsap.registerPlugin(ScrollTrigger);
+  const headlineWidth = $(".headline-content h1").outerWidth();
+
+  const viewportWidth = $(window).width();
+
+  const translateAmount = headlineWidth - viewportWidth + 50; // 50px offset
 
   const t1 = gsap.timeline({
     scrollTrigger: {
-      trigger: ".features",
-      start: "bottom center",
-      end: "bottom top",
+      trigger: ".headline",
+      start: "top center",
+      end: "+=100%",
       scrub: 1,
+      pin: true,
       ease: "ease-in-out",
-      // markers: {
-      //   startColor: "green",
-      //   endColor: "yellow",
-      // },
+      markers: {
+        startColor: "green",
+        endColor: "yellow",
+      },
     },
   });
 
-  t1.fromTo(".headline-content h1", { x: 50, y: 0 }, { x: -300, y: -20 });
+  t1.fromTo(".headline-content h1", { x: "50px" }, { x: `-${translateAmount}px` });
 });
